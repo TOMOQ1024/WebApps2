8c26e461d10df7192fd39f095b1b894be9ad8ca1
TOMOQ1024 <tomo_suzu_16384@au.com>
2023-10-07 07:31:06 +0900

    HowTo画面キャラクタ設置

--- src/app/games/flappypigeon/Render/DrawHowto.ts
+++ src/app/games/flappypigeon/Render/DrawHowto.ts
@@ -8,6 +8,8 @@ export default function DrawHowto(game: Game){
   ctx.font = `${Params.CANVASHEIGHT/20}px serif`;
   ctx.fillText('上下から伸びる・に触れないように，・をゴールまで導こう！', Params.CANVASWIDTH/2, Params.CANVASHEIGHT*.3, Params.CANVASWIDTH*0.8);
   ctx.fillText(`${Params.TIMELIMIT/1000}秒のあいだなら何度でも挑戦できるよ！`, Params.CANVASWIDTH/2, Params.CANVASHEIGHT*.4, Params.CANVASWIDTH*0.8);
+  DrawImgAt(game, 'nh', 2.64, 2.3, 3, .35);
+  DrawImgAt(game, 'f0', 5.54, 2.3, 0, .35);
   ctx.fillText(`好きなキーで操作．初回操作と同時にタイマースタート`, Params.CANVASWIDTH/2, Params.CANVASHEIGHT*.5, Params.CANVASWIDTH*0.8);
 
   ctx.font = `${Params.CANVASHEIGHT/30}px serif`;
