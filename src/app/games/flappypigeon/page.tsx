380e5a41a0be677b5712e18be8dc818547fc7aaa
TOMOQ1024 <tomo_suzu_16384@au.com>
2023-10-07 11:43:09 +0900

    フレームレート再修正

--- src/app/games/flappypigeon/page.tsx
+++ src/app/games/flappypigeon/page.tsx
@@ -20,7 +20,7 @@ export default function Main(){
       Update(game);
       Render(game);
       // requestAnimationFrame(GameLoop);
-      setTimeout(GameLoop, Params.FRAMERATE);
+      setTimeout(GameLoop, 1000/Params.FRAMERATE);
     }
     GameLoop();
 
