2bb03fa50e9ecfed89c514c9ffe0f675b7e019d3
TOMOQ1024 <tomo_suzu_16384@au.com>
2023-11-13 08:56:55 +0900

    Parserの移植

---
+++ src/src/parser/Main.ts
@@ -0,0 +1,46 @@
+import { ExprType, NullParseResult, ParseResult } from "./Def";
+import { CStack } from "./CStack";
+import { BNode } from "./Node";
+import Parser from "./Parser";
+
+export function Parse(input: string, dvn: string[]): ParseResult {
+  let parser = new Parser();
+  let n: number;
+  let result: (BNode|null);
+  let cst: (CStack|undefined);
+
+  let eType: ExprType;
+
+  if(!input.match(/[<>=]/)){
+    return NullParseResult;
+  } else if(input.match(/[><]/)){
+    eType = 'ineq';
+  } else {
+    eType = 'defi';
+  }
+
+  console.clear();
+  console.log(`input: "${input}"`);
+  try {
+    n = performance.now();
+    result = parser.parse(input, eType, dvn);
+    if(!result)return NullParseResult;
+    console.log(result.toStr());
+    console.log(`parse end in ${performance.now()-n}ms`);
+    cst = new CStack(result, eType);
+    console.log(cst.togl(cst.root));
+  } catch (e) {
+    console.error(e);
+    return NullParseResult;
+  } finally {
+    if(cst === undefined){
+      return NullParseResult;
+    }
+    return ({
+      status: true,
+      type: input.match(/=/) ? 'defi' : 'ineq',
+      result: cst.togl(cst.root)
+    });
+  }
+  
+}
