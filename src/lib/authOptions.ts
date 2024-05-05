b54001c58240b537d71272ebbd8cdbf212005844
TOMOQ1024 <tomo_suzu_16384@au.com>
2024-05-05 10:22:22 +0900

    remove some log

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -19,8 +19,6 @@ export const authOptions = {
       authorize: async (credentials, req) => {
         const hash = createHash('sha512');
         console.log(`api_base_url: ${process.env.VERCEL_URL || process.env.LOCAL_API_BASE_URL}`);
-        console.log(`username: ${credentials!.username}`);
-        console.log(`passhash: ${hash.update(credentials!.password).digest('hex')}`);
         return axios.get(`https://${process.env.VERCEL_URL || process.env.LOCAL_API_BASE_URL}/api/get-user`, {
           headers: {
             'username': credentials!.username,
