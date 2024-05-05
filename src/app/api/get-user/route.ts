b9e06d0dc191a357f224c8f77d264148b908c90c
TOMOQ1024 <tomo_suzu_16384@au.com>
2024-05-05 10:41:39 +0900

    log findMany

--- src/app/api/get-user/route.ts
+++ src/app/api/get-user/route.ts
@@ -9,6 +9,7 @@ export async function GET(
     const username = req.headers.get('username') || '';
     const passhash = req.headers.get('passhash') || '';
     console.log(username, passhash, prisma);
+    console.log(`findmany: ${await prisma.user.findMany()}`);
     const user = await prisma.user.findFirst({
       where: {
         name: username,
