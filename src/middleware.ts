ac38020cf1802f5e4710378d7a7d26665fc9f9bb
TOMOQ1024 <tomo_suzu_16384@au.com>
2024-07-01 15:21:23 +0900

    middlewareのURLに関する変更#167

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,23 +5,24 @@ import { NextRequest } from "next/server";
 export default withAuth({
   secret: process.env.AUTH_SECRET,
   callbacks: {
-    authorized({ req , token }: {
-      req: NextRequest;
-      token: JWT | null;
-    }) {
+    authorized({ req, token }: { req: NextRequest; token: JWT | null }) {
       // 認証済のユーザと自分のサーバーからのリクエストのみアクセス可能に
-      if(token) return true // If there is a token, the user is authenticated
-      
-      const referer = req.headers.get('referer');
-      if (referer && referer.startsWith(process.env.NEXTAUTH_URL!)) {
+      if (token) return true; // If there is a token, the user is authenticated
+
+      const referer = req.headers.get("referer");
+      console.log(referer, process.env.NEXTAUTH_URL, process.env.VERCEL_URL);
+      if (
+        referer &&
+        referer.startsWith(process.env.NEXTAUTH_URL ?? process.env.VERCEL_URL!)
+      ) {
         return true;
       } else {
         return false;
       }
-    }
+    },
   },
 });
 
 export const config = {
-  matcher: '/private/:path*',
-}
+  matcher: "/private/:path*",
+};
