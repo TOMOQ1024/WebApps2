6460d2ac89727a335b8c03d8e482eaab5822959e
TOMOQ1024 <tomo_suzu_16384@au.com>
2024-09-17 09:59:42 +0900

    ビルド時のエラー対処

--- src/app/api/dbtest/create/route.ts
+++ src/app/api/dbtest/create/route.ts
@@ -1,32 +1,31 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-import { authOptions } from '@/lib/authOptions';
-import { getServerSession } from 'next-auth';
+import { authOptions } from "@/lib/authOptions";
+import { getServerSession } from "next-auth";
 
-export async function POST(
-  req: NextRequest,
-  res: NextResponse
-) {
+export async function POST(req: NextRequest, res: NextResponse) {
   try {
     const session = await getServerSession(authOptions);
     if (!session) {
-      throw new Error('Unauthorized');
+      throw new Error("Unauthorized");
     }
-    const { expression } = await req.json();
-    const data = await prisma.post.create({
+    const { authorId, expression } = await req.json();
+    const data = await prisma.compDynamPost.create({
       select: {
         id: true,
+        authorId: true,
         expression: true,
         // title: true,
         // description: true,
       },
       data: {
+        authorId: authorId,
         expression: expression,
       },
     });
-    
+
     return NextResponse.json({ data });
   } catch (error) {
-    return NextResponse.json({ error }, {status: 401});
+    return NextResponse.json({ error }, { status: 401 });
   }
 }
