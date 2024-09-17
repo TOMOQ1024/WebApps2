a25c25ffedf65facd2a0351cda3c373e192e0cf9
TOMOQ1024 <tomo_suzu_16384@au.com>
2024-09-17 10:10:08 +0900

    Revert "ビルド時のエラー対処"
    This reverts commit 6460d2ac89727a335b8c03d8e482eaab5822959e.

--- src/app/api/dbtest/delete/route.ts
+++ src/app/api/dbtest/delete/route.ts
@@ -1,23 +1,26 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 import prisma from "@/lib/prisma";
-import { authOptions } from "@/lib/authOptions";
-import { getServerSession } from "next-auth";
+import { authOptions } from '@/lib/authOptions';
+import { getServerSession } from 'next-auth';
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(
+  req: NextRequest,
+  res: NextResponse
+) {
   try {
     const session = await getServerSession(authOptions);
     if (!session) {
-      throw new Error("Unauthorized");
+      throw new Error('Unauthorized');
     }
     const { id } = await req.json();
-    const data = await prisma.compDynamPost.delete({
+    const data = await prisma.post.delete({
       where: {
         id: parseInt(id, 10),
       },
     });
-
+    
     return NextResponse.json({ data });
   } catch (error) {
-    return NextResponse.json({ error }, { status: 401 });
+    return NextResponse.json({ error }, {status: 401});
   }
 }
--- src/app/api/dbtest/get/route.ts
+++ src/app/api/dbtest/get/route.ts
@@ -1,18 +1,21 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 import prisma from "@/lib/prisma";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/authOptions";
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/authOptions';
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(
+  req: NextRequest,
+  res: NextResponse
+) {
   try {
     const session = await getServerSession(authOptions);
     if (!session) {
-      throw new Error("Unauthorized");
+      throw new Error('Unauthorized');
     }
-    const data = await prisma.compDynamPost.findMany();
-
+    const data = await prisma.post.findMany();
+    
     return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json({ error }, { status: 401 });
+    return NextResponse.json({ error }, {status: 401});
   }
 }
