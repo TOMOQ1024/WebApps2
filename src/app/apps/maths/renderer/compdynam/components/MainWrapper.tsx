7978b44fff6ec41508c60e2f10211976aa790011
TOMOQ1024 <tomo_suzu_16384@au.com>
2024-09-23 18:47:57 +0900

    キーイベントの追加#69

--- src/app/apps/maths/renderer/compdynam/components/MainWrapper.tsx
+++ src/app/apps/maths/renderer/compdynam/components/MainWrapper.tsx
@@ -3,17 +3,118 @@ import { useEffect, useState } from "react";
 import Core from "../Core";
 import { Vector2 } from "three";
 import Controls from "./Controls";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
 
 export default function MainWrapper() {
+  const session = useSession();
+  const searchParams = useSearchParams();
   const [core, setCore] = useState<Core>();
+  const [isFull, setIsFull] = useState(false);
 
   useEffect(() => {
+    if (searchParams) {
+      let v = searchParams.get("nessy");
+      if (v !== null && core) {
+        core.nessyMode = true;
+      }
+    }
+
     if (!core) {
       const initCore = new Core();
       setCore(initCore);
 
+      if (searchParams) {
+        let v = searchParams.get("nessy");
+        if (v !== null) {
+          initCore.nessyMode = true;
+        }
+      }
+
       const HandleResize = () => {
         initCore.resizeCanvas();
+      };
+
+      const HandleKeyDown = (e: KeyboardEvent) => {
+        // フルスクリーン切り替え
+        const tagName = (e.target as HTMLElement).tagName;
+        if (e.key === "f" && !e.shiftKey && !e.metaKey && tagName !== "INPUT") {
+          const wr = initCore.cvs.parentElement!;
+          if (!document.fullscreenElement) {
+            setIsFull(true);
+            wr.requestFullscreen();
+            HandleResize();
+          } else {
+            setIsFull(false);
+            document.exitFullscreen();
+            HandleResize();
+          }
+        }
+        if (session && e.key === ";" && !e.shiftKey && !e.metaKey) {
+          const data = {
+            authorId: 1,
+            z0Expression: initCore.z0expr,
+            expression: initCore.funcexpr,
+            radius: initCore.graph.radius,
+            originX: initCore.graph.origin.x,
+            originY: initCore.graph.origin.y,
+            tags: [],
+          };
+          (async () => {
+            const response = await axios.post("/api/works/create", data);
+            console.log(response);
+          })();
+        }
+        if (0 && e.key === "v" && !e.shiftKey && !e.metaKey) {
+          // 仮
+          const ipt = document.querySelector("#func-input") as HTMLInputElement;
+          console.log(ipt);
+
+          const stream = initCore.cvs.captureStream();
+          const recorder = new MediaRecorder(stream, {
+            mimeType: "video/mp4",
+            videoBitsPerSecond: 2500000,
+          });
+          const anchor = document.createElement("a");
+          anchor.innerText = "download";
+          anchor.style.display = "none";
+          ipt.parentNode?.appendChild(anchor);
+
+          recorder.ondataavailable = (e) => {
+            const videoBlob = new Blob([e.data], { type: e.data.type });
+            const blobUrl = window.URL.createObjectURL(videoBlob);
+            anchor.download = "movie.mp4";
+            anchor.href = blobUrl;
+            anchor.style.display = "block";
+          };
+
+          // const C1 = '-0.2-0.7i';// c
+          const C1 = "-0.63i";
+          // const C2 = '-0.2-0.7i';
+          // const C2 = '-0.6-0.42i';
+          const C2 = "-0.8";
+          ipt.value = `z^2+mix(c,-z,0.00)`;
+          ipt.click();
+          recorder.start();
+
+          let t = 0;
+          const itv = setInterval(() => {
+            const T = t * t * (3 - 2 * t);
+
+            ipt.value = `z^2+mix(c,-z,${T})`;
+            ipt.click();
+            // ipt.innerHTML = `z^2+mix(c,-0.2-0.7i,${T.toFixed(2)})`;
+            ipt.value = `z^2+mix(c,-z,${T.toFixed(2)})`;
+
+            if (1 < t) {
+              clearInterval(itv);
+              recorder.stop();
+            }
+
+            t += 0.005;
+          }, 100);
+        }
       };
 
       const HandleWheel = (e: WheelEvent) => {
@@ -112,6 +213,7 @@ export default function MainWrapper() {
       (async () => {
         await initCore.init();
 
+        document.addEventListener("keydown", HandleKeyDown);
         cvs.addEventListener("pointerdown", HandlePointerDown, {
           passive: false,
         });
@@ -124,6 +226,7 @@ export default function MainWrapper() {
       })();
       return () => {
         initCore.endLoop();
+        document.removeEventListener("keydown", HandleKeyDown);
         cvs.removeEventListener("pointerdown", HandlePointerDown);
         cvs.removeEventListener("pointermove", HandlePointerMove);
         cvs.removeEventListener("pointerup", HandlePointerUp);
