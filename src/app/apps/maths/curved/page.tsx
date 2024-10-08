51777b6e3d95c9d9158610227cde43d0a9b4a3c0
TOMOQ1024 <tomo_suzu_16384@au.com>
2024-10-08 11:00:09 +0900

    Three.jsでの基礎の書き直し#95

---
+++ src/app/apps/maths/curved/page.tsx
@@ -0,0 +1,10 @@
+import "./app.scss";
+import MainWrapper from "./components/MainWrapper";
+
+export const metadata = {
+  title: "Curved",
+};
+
+export default function Main() {
+  return <MainWrapper />;
+}
--- src/app/curved/Mouse.ts
+++
@@ -1,9 +0,0 @@
-import Vec2 from "@/src/Vec2";
-
-export default class MouseMgr {
-  isDown = false;
-  dPos = new Vec2();
-  pos = new Vec2();
-
-  constructor(){}
-}
